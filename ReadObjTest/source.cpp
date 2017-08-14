2cc395fa10db9c93008fd4c78af187e53d9ef97d
xiaomingmai <499466765@qq.com>
2017-08-14 12:11:06 +0800

    加入fstream

--- ReadObjTest/source.cpp
+++ ReadObjTest/source.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
+#include<string>   
+
+#include<fstream>
+#include <sstream>    //istringstream
+
 using namespace std;
 
+
+string g_str;
+ifstream g_in;
+
 int main(){
-	cout<<"hello"<<endl;
+	g_in.open("D:\\cube.obj");
+
+	while(getline(g_in, g_str)){
+		istringstream in(g_str);
+	}
+
 	system("pause");
-	cout<<"hhhhh"<<endl;
 	return 0;
 
 }
